3d9b9857220f6b3d4ecae62a35e982d650a2ae19
Justin Cheng <jucheng.ca@gmail.com>
2023-05-19 22:49:55 -0700

    ports

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = 8080;
 const routes = require('./routes.js');
 
 app.get('/', (req, res) => {

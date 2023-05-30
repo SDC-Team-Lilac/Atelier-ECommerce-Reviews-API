3390b7955b9ddd56eddc32e67de35abd3355398a
Justin Cheng <jucheng.ca@gmail.com>
2023-05-29 17:33:30 -0700

    url

--- server/controllers.js
+++ server/controllers.js
@@ -28,10 +28,7 @@ module.exports = {
     }
 
     const client = redis.createClient({
-      socket: {
-        port: 6379,
-        host: 'client'
-     },
+      url: 'redis://redis:6379',
       legacyMode: true,
     });
 
